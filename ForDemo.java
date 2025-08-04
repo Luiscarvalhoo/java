f90c23571224df3efbbae6334935451950f3338e
Luis Felipe Carvalho Lima <cluisfelipe063@gmail.com>
2025-08-04 19:05:25 -0300

    Create ForDemo.java
    Add codigo da faculdade

---
+++ ForDemo.java
@@ -0,0 +1,13 @@
+public class ForDemo {
+
+    public static void main(String[] args) {
+
+        int count;
+
+        for(count = 0; count < 6;count++)  
+            System.out.println("This is count: " + count);
+
+        System.out.println("Done!");
+    }
+    
+}
