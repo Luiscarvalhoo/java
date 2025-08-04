f4bc29e00e4b07b078aff90577a08eb4faf3e80d
Luis Felipe Carvalho Lima <cluisfelipe063@gmail.com>
2025-08-04 19:06:01 -0300

    Create GalToLit.java
    Add o codigo da faculdade

---
+++ GalToLit.java
@@ -0,0 +1,14 @@
+class GalToLit  {
+    public static void main(String[] args ) {
+
+        double gallors;
+        double litters;
+
+
+        gallors = 10;
+
+        litters = gallors * 3.7854;
+
+        System.out.println(gallors + " gallons is " + litters + " liters");
+    }
+}
